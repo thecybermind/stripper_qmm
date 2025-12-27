1130046fd65e33eadf6e29e3396fa7c1a9acb053
Kevin Masterson <k.m.masterson@gmail.com>
2025-12-28 01:25:36 +0200

    re-add map ini files to package

--- .github/build/linux/package.sh
+++ .github/build/linux/package.sh
@@ -4,6 +4,8 @@ cd package
 rm -f *
 cp ../README.md ./
 cp ../LICENSE ./
+cp ../global.ini ./
+cp ../q3dm1.ini ./
 
 for f in COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET; do
   cp ../bin/release-$f/x86/stripper_qmm_$f.so ./
