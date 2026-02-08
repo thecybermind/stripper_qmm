d7d23f1a5f7d01523bb5f99525f309d5e363573c
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 22:26:51 +0200

    add sof2sp to build files

--- .github/build/linux/package.sh
+++ .github/build/linux/package.sh
@@ -7,7 +7,7 @@ cp ../LICENSE ./
 cp ../global.ini ./
 cp ../q3dm1.ini ./
 
-for f in COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET; do
+for f in COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET; do
   cp ../bin/release-$f/x86/stripper_qmm_$f.so ./
   cp ../bin/release-$f/x86_64/stripper_qmm_x86_64_$f.so ./
 done
--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -4,7 +4,7 @@ del /q *
 rem copy ..\README.md .\
 rem copy ..\LICENSE .\
 
-for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET) do (
+for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
     copy ..\bin\Release-%%x\x86\stripper_qmm_%%x.dll .\
     copy ..\bin\Release-%%x\x64\stripper_qmm_x86_64_%%x.dll .\     
 )
